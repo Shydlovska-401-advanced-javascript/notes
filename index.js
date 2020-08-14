e4b11f02f0fe5389ed9d3c04161520e370b316dd
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-08-13 21:09:34 -0700

    refactoring code

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 'use strict';
-
-
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/notesy', 
+{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
 
 
 const Input = require('./lib/input.js');
@@ -10,8 +13,5 @@ const Notes = require('./lib/notes.js');
 
 const options = new Input();
 const notes = new Notes();
-
-
-
-
-notes.execute(options);
+// () => 
+notes.execute(options).then(()=> mongoose.disconnect());
