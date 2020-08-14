a08ee5d175f994f245cd78877009adbe31f29ee5
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-08-13 19:57:28 -0700

    earlier version of the code

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 'use strict';
 
+
+
+
+
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 const options = new Input();
 const notes = new Notes();
+
+
+
 
 notes.execute(options);
